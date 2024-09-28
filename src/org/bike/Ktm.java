cce3ab8e03170a605b17c06d44a1b5a57dd0a80d
Bibin <bibinjoseraj@gmail.com>
2024-09-28 10:45:34 +0530

    New File added

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,34 @@
+package org.bike;
+
+public class Ktm implements Bike {
+
+	@Override
+	public void cost() {
+		System.out.println("1.5L");
+		
+	}
+
+	@Override
+	public void speed() {
+		System.out.println("210cc");
+		
+	}
+	
+	public void cost1() {
+		System.out.println("2L");
+		
+	}
+	public void speed1() {
+		System.out.println("350cc");
+		
+	}
+	public static void main(String[] args) {
+		Ktm k=new Ktm();
+		k.cost1();
+		k.speed1();
+		
+		Bike b=new Ktm();
+		b.cost();
+		b.speed();
+	}
+}
